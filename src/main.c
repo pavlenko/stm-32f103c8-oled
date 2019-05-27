4108f124a34c970f6d66163056bfb0ce5bbf7d23
Pavlenko Eugeniy <pavlenko.obs@gmail.com>
2019-05-27 21:35:40 +0300

    add i2c init

--- src/main.c
+++ src/main.c
@@ -1,7 +1,10 @@
 #include "main.h"
 
+I2C_HandleTypeDef i2c2;
+
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_I2C2_Init(void);
 
 int main(void)
 {
@@ -13,6 +16,7 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+    MX_I2C2_Init();
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
@@ -81,6 +85,57 @@ static void MX_GPIO_Init(void)
 
     HAL_GPIO_Init(GPIOC, &gpio);
 
+}
+
+static void MX_I2C2_Init(void)
+{
+    i2c2.Instance             = I2C2;
+    i2c2.Init.ClockSpeed      = 400000;
+    i2c2.Init.DutyCycle       = I2C_DUTYCYCLE_2;
+    i2c2.Init.OwnAddress1     = 0;
+    i2c2.Init.AddressingMode  = I2C_ADDRESSINGMODE_7BIT;
+    i2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+    i2c2.Init.OwnAddress2     = 0;
+    i2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+    i2c2.Init.NoStretchMode   = I2C_NOSTRETCH_DISABLE;
+
+    HAL_I2C_Init(&i2c2);
+}
+
+void HAL_I2C_MspInit(I2C_HandleTypeDef* i2c)
+{
+    GPIO_InitTypeDef GPIO_InitStruct;
+
+    if (i2c->Instance == I2C2) {
+        /**
+         * I2C2 GPIO Configuration
+         * PB10 ------> I2C2_SCL
+         * PB11 ------> I2C2_SDA
+         */
+        GPIO_InitStruct.Pin   = GPIO_PIN_10|GPIO_PIN_11;
+        GPIO_InitStruct.Mode  = GPIO_MODE_AF_OD;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+
+        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+        /* Peripheral clock enable */
+        __HAL_RCC_I2C2_CLK_ENABLE();
+    }
+}
+
+void HAL_I2C_MspDeInit(I2C_HandleTypeDef* i2c)
+{
+    if (i2c->Instance == I2C2) {
+        __HAL_RCC_I2C2_CLK_DISABLE();
+
+        /**
+         * I2C2 GPIO Configuration
+         * PB10 ------> I2C2_SCL
+         * PB11 ------> I2C2_SDA
+         */
+        HAL_GPIO_DeInit(GPIOB, GPIO_PIN_10|GPIO_PIN_11);
+
+    }
 }
 
 /**
