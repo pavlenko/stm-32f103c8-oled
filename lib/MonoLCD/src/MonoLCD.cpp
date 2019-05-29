0a0c4fe584f6484860abe52ab4782dde37943f97
Pavlenko Eugeniy <pavlenko.obs@gmail.com>
2019-05-29 10:39:38 +0300

    add draw methods draft

--- lib/MonoLCD/src/MonoLCD.cpp
+++ lib/MonoLCD/src/MonoLCD.cpp
@@ -9,14 +9,29 @@ MonoLCD::MonoLCD(uint16_t width, uint16_t height) {
     this->_buffer = buffer;
 }
 
-void MonoLCD::drawPixel(uint16_t x, uint16_t y, MonoLCD_COLOR_t color) {
-    if (x >= this->_width || y >= this->_height) {
-        return;
-    }
+void MonoLCD::pixel(uint16_t x, uint16_t y, MonoLCD_COLOR_t color) {
+    if (x >= this->_width || y >= this->_height) return;
 
     if (color == MonoLCD_COLOR_WHITE) {
         this->_buffer[x + (y / 8) * this->_width] |= 1u << (y % 8u);
     } else {
         this->_buffer[x + (y / 8) * this->_width] &= ~(1u << (y % 8u));
     }
+}
+
+//TODO
+void MonoLCD::line(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, MonoLCD_COLOR_t color) {
+    if (x0 >= this->_width) x0 = this->_width - 1;
+    if (x1 >= this->_width) x1 = this->_width - 1;
+    if (y0 >= this->_height) y0 = this->_height - 1;
+    if (y1 >= this->_height) y1 = this->_height - 1;
+
+    int16_t dx = (x0 < x1) ? (x1 - x0) : (x0 - x1);
+    int16_t dy = (y0 < y1) ? (y1 - y0) : (y0 - y1);
+
+    if (dx == 0) {/*vertical line*/return;}
+
+    if (dy == 0) {/*horizontal line*/return;}
+
+    /*diagonal line*/
 }
--- lib/MonoLCD/src/MonoLCD.h
+++ lib/MonoLCD/src/MonoLCD.h
@@ -25,7 +25,33 @@ public:
      * @param y
      * @param color
      */
-    void drawPixel(uint16_t x, uint16_t y, MonoLCD_COLOR_t color);
+    void pixel(uint16_t x, uint16_t y, MonoLCD_COLOR_t color);
+
+    /**
+     * @param x0
+     * @param y0
+     * @param x1
+     * @param y1
+     * @param color
+     */
+    void line(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, MonoLCD_COLOR_t color);
+
+    /**
+     * @param x0
+     * @param y0
+     * @param x1
+     * @param y1
+     * @param color
+     */
+    void rect(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, MonoLCD_COLOR_t color);
+
+    /**
+     * @param cx
+     * @param cy
+     * @param r
+     * @param color
+     */
+    void circle(uint16_t cx, uint16_t cy, uint16_t r, MonoLCD_COLOR_t color);
 };
 
 #endif //MONO_LCD_H
