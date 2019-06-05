5c35cfedd6ccad7e775f1a7d42216d1a180defe3
Pavlenko Eugeniy <pavlenko.obs@gmail.com>
2019-06-05 17:32:24 +0300

    add ticker structure

--- lib/Ticker/src/Ticker.h
+++ lib/Ticker/src/Ticker.h
@@ -9,6 +9,19 @@ extern "C" {
 
 typedef void (*Ticker_Handler_t) ();
 
+typedef struct {
+    uint32_t interval;
+    void (*callback) ();
+} Ticker_Timer_t;
+
+typedef struct {
+    uint32_t        currentTime;
+    Ticker_Timer_t *repeatedList;
+    uint8_t         repeatedSize;
+    Ticker_Timer_t *singularList;
+    uint8_t         singularSize;
+} Ticker_Handle_t;
+
 void Ticker_initialize();
 
 void Ticker_setSingular(uint32_t intervalMS, Ticker_Handler_t handler);
