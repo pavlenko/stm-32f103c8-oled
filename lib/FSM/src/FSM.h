ed6fd488a7df29fa83eb5972305bde98807a7345
Pavlenko Eugeniy <pavlenko.obs@gmail.com>
2019-06-05 08:57:42 +0300

    start create finite state machine lib

---
+++ lib/FSM/src/FSM.h
@@ -0,0 +1,14 @@
+#ifndef FSM_H
+#define FSM_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//TODO create finite state machine api for allow display state management and maybe something else
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //FSM_H
