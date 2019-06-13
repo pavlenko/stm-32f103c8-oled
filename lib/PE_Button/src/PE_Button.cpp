84c192372aff17482a27f7db00bef25719bcf7f2
Pavlenko Eugeniy <pavlenko.obs@gmail.com>
2019-06-13 11:34:47 +0300

    add dispatch logic

--- lib/PE_Button/src/PE_Button.cpp
+++ lib/PE_Button/src/PE_Button.cpp
@@ -7,8 +7,6 @@
 #define PE_BUTTON_BIT_SET(_byte_, _bit_) ((_byte_) |= (1UL << (_bit_)))
 #define PE_BUTTON_BIT_CLR(_byte_, _bit_) ((_byte_) &= ~(1UL << (_bit_)))
 
-uint8_t state;
-
 PE_Button::PE_Button(PE_ButtonReader_t reader) {
     _reader = reader;
 }
@@ -19,6 +17,8 @@ void PE_Button::initialize(uint32_t time) {
 
 void PE_Button::dispatch(uint32_t time) {
     _time = time;
+
+    PE_ButtonState_t state;
 
     // Save the previous value
     if (PE_BUTTON_BIT_GET(_status, PE_BUTTON_STATE_CURRENT)) {
@@ -34,35 +34,35 @@ void PE_Button::dispatch(uint32_t time) {
         PE_BUTTON_BIT_CLR(_status, PE_BUTTON_STATE_CURRENT);
     }
 
-    /*if (BUTTON_BIT_READ(_state, BUTTON_BIT_CURRENT) != BUTTON_BIT_READ(_state, BUTTON_BIT_PREVIOUS)) {
+    if (PE_BUTTON_BIT_GET(_status, PE_BUTTON_STATE_CURRENT) != PE_BUTTON_BIT_GET(_status, PE_BUTTON_STATE_PREVIOUS)) {
         // If state changed - reset counter and return unknown state
         _counter = 0;
 
         // Clear callback triggered flags
-        BUTTON_BIT_CLEAR(_state, BUTTON_BIT_ON_PRESS);
-        BUTTON_BIT_CLEAR(_state, BUTTON_BIT_ON_RELEASE);
-        BUTTON_BIT_CLEAR(_state, BUTTON_BIT_ON_HOLD);
+        PE_BUTTON_BIT_CLR(_status, BUTTON_BIT_ON_PRESS);
+        PE_BUTTON_BIT_CLR(_status, BUTTON_BIT_ON_RELEASE);
+        PE_BUTTON_BIT_CLR(_status, BUTTON_BIT_ON_HOLD);
 
-        return BUTTON_STATE_UNKNOWN;
+        state = PE_BUTTON_STATE_UNKNOWN;
     } else {
         if (_counter < sizeof(uint32_t)) {
             _counter++;
         }
 
-        if (BUTTON_BIT_READ(_state, BUTTON_BIT_CURRENT)) {
+        if (PE_BUTTON_BIT_GET(_status, PE_BUTTON_STATE_CURRENT)) {
             if (_counter < BUTTON_DEBRIEF_THRESHOLD) {
                 // If counter less than debrief threshold - return unknown state
-                return BUTTON_STATE_UNKNOWN;
+                state = PE_BUTTON_STATE_UNKNOWN;
+            } else {
+                state = PE_BUTTON_STATE_PRESSED;
             }
-
-            return BUTTON_STATE_PRESSED;
         } else {
             if (_counter < BUTTON_DEBRIEF_THRESHOLD) {
                 // If counter less than debrief threshold - return unknown state
-                return BUTTON_STATE_UNKNOWN;
+                state = PE_BUTTON_STATE_UNKNOWN;
+            } else {
+                state = PE_BUTTON_STATE_RELEASED;
             }
-
-            return BUTTON_STATE_RELEASED;
         }
-    }*/
+    }
 }
