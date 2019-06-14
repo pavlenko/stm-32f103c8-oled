50e68b1e2976445258647091d9c1fb8d4f8b42da
Pavlenko Eugeniy <pavlenko.obs@gmail.com>
2019-06-14 10:18:45 +0300

    complete button lib, need test

--- lib/PE_Button/src/PE_Button.cpp
+++ lib/PE_Button/src/PE_Button.cpp
@@ -1,10 +1,5 @@
 #include "PE_Button.h"
 
-#define TIMEOUT_DEBRIEF       10U
-#define TIMEOUT_HOLD_SINGULAR 1000U
-#define TIMEOUT_HOLD_REPEATED 1200U
-#define TIMEOUT_REPEAT        100U
-
 #define STATUS_CURRENT_Pos (0U)
 #define STATUS_CURRENT_Msk (1U << STATUS_CURRENT_Pos)
 #define STATUS_CURRENT     STATUS_CURRENT_Msk
@@ -25,22 +20,15 @@
 #define STATUS_TRIG_HOLD_Msk (1U << STATUS_TRIG_HOLD_Pos)
 #define STATUS_TRIG_HOLD     STATUS_TRIG_HOLD_Msk
 
-#define PE_BUTTON_BIT_GET(_byte_, _bit_) (((_byte_) >> (_bit_)) & 0x01)
-#define PE_BUTTON_BIT_SET(_byte_, _bit_) ((_byte_) |= (1UL << (_bit_)))
-#define PE_BUTTON_BIT_CLR(_byte_, _bit_) ((_byte_) &= ~(1UL << (_bit_)))
-
 PE_Button::PE_Button(PE_ButtonReader_t reader) {
     _reader = reader;
 }
 
-void PE_Button::initialize(uint32_t time) {
-    _time = time;
+void PE_Button::initialize(uint32_t millis) {
+    _millis = millis;
 }
 
-void PE_Button::dispatch(uint32_t time) {
-    uint8_t position;
-    void (*handler) ();
-
+void PE_Button::dispatch(uint32_t millis) {
     // Save previous state
     if (_status & STATUS_CURRENT) {
         _status |= STATUS_PREVIOUS;
@@ -51,42 +39,67 @@ void PE_Button::dispatch(uint32_t time) {
     // Read current state and prefetch handlers & flag positions
     if (_reader()) {
         _status |= STATUS_CURRENT;
-        position = STATUS_TRIG_PRESS_Pos;
-        handler  = _onPress;
     } else {
         _status &= ~STATUS_CURRENT;
-        position = STATUS_TRIG_RELEASE_Pos;
-        handler  = _onRelease;
     }
 
     // Reset execution flags if state changed
     if ((_status & STATUS_CURRENT) != (_status & STATUS_PREVIOUS)) {
-        _time = time;
+        _millis = millis;
         _status &= ~(STATUS_TRIG_PRESS | STATUS_TRIG_RELEASE | STATUS_TRIG_HOLD);
         return;
     }
 
-    // Process repeated handler
-    if (time - _time > TIMEOUT_HOLD_REPEATED) {
-        _time = time - TIMEOUT_REPEAT;
-        _onHoldRepeated();
-        return;
+    // Process pressed/released handler, must be first because must called always
+    if (millis - _millis > PE_BUTTON_TIMEOUT_DEBRIEF) {
+        if ((_status & STATUS_CURRENT) == 1U) {
+            if ((_status & (1U << STATUS_TRIG_PRESS)) == 0U) {
+                _status |= (1U << STATUS_TRIG_PRESS);
+                if (_onPress) _onPress();
+            }
+        } else {
+            if ((_status & (1U << STATUS_TRIG_RELEASE)) == 0U) {
+                _status |= (1U << STATUS_TRIG_RELEASE);
+                if (_onRelease) _onRelease();
+            }
+        }
     }
 
+#if PE_BUTTON_USE_HOLD_SINGULAR == 1
     // Process singular handler
-    if (time - _time > TIMEOUT_HOLD_SINGULAR) {
-        if ((_status & STATUS_TRIG_HOLD) == 0) {
+    if (millis - _millis > PE_BUTTON_TIMEOUT_HOLD_SINGULAR) {
+        if ((_status & STATUS_TRIG_HOLD) == 0U) {
             _status |= STATUS_TRIG_HOLD;
-            _onHoldSingular();
-            return;
+            if (_onHoldSingular) _onHoldSingular();
         }
     }
+#endif
 
-    // Process pressed/released handler
-    if (time - _time > TIMEOUT_DEBRIEF) {
-        if ((_status & (1U << position)) == 0) {
-            _status |= (1U << position);
-            handler();
-        }
+#if PE_BUTTON_USE_HOLD_REPEATED == 1
+    // Process repeated handler
+    if (millis - _millis > PE_BUTTON_TIMEOUT_HOLD_REPEATED) {
+        _millis = millis - PE_BUTTON_TIMEOUT_REPEAT;
+        if (_onHoldRepeated) _onHoldRepeated();
     }
+#endif
 }
+
+void PE_Button::setOnPress(PE_ButtonHandler_t handler) {
+    _onPress = handler;
+}
+
+void PE_Button::setOnRelease(PE_ButtonHandler_t handler) {
+    _onRelease = handler;
+}
+
+#if PE_BUTTON_USE_HOLD_SINGULAR == 1
+void PE_Button::setOnHoldSingular(PE_ButtonHandler_t handler) {
+    _onHoldSingular = handler;
+}
+#endif
+
+#if PE_BUTTON_USE_HOLD_REPEATED == 1
+void PE_Button::setOnHoldRepeated(PE_ButtonHandler_t handler) {
+    _onHoldRepeated = handler;
+}
+#endif
--- lib/PE_Button/src/PE_Button.h
+++ lib/PE_Button/src/PE_Button.h
@@ -3,46 +3,94 @@
 
 #include <stdint.h>
 
-//TODO click handle
-//TODO execute checks only if handler configured
-//TODO allow configure timeouts globally for all buttons
-
-#ifndef PE_BUTTON_THRESHOLD_DEBRIEF
-#define PE_BUTTON_THRESHOLD_DEBRIEF 10U
+#ifndef PE_BUTTON_USE_HOLD_SINGULAR
+#define PE_BUTTON_USE_HOLD_SINGULAR 1
 #endif
 
-#ifndef PE_BUTTON_THRESHOLD_HOLD
-#define PE_BUTTON_THRESHOLD_HOLD 1000U
+#ifndef PE_BUTTON_USE_HOLD_REPEATED
+#define PE_BUTTON_USE_HOLD_REPEATED 1
 #endif
 
-// Timeout for execute repeated hold callback, must match condition: <val> > (<holdMS> - <debriefMS>) * 2
-#ifndef PE_BUTTON_TIMEOUT_REPEATED
-#define PE_BUTTON_TIMEOUT_REPEATED 250U
+#ifndef PE_BUTTON_TIMEOUT_DEBRIEF
+#define PE_BUTTON_TIMEOUT_DEBRIEF 10U
 #endif
 
-typedef enum {
-    PE_BUTTON_STATE_RELEASED = -1,
-    PE_BUTTON_STATE_UNKNOWN  =  0,
-    PE_BUTTON_STATE_PRESSED  =  1,
-} PE_ButtonState_t;
+#ifndef PE_BUTTON_TIMEOUT_HOLD_SINGULAR
+#define PE_BUTTON_TIMEOUT_HOLD_SINGULAR 1000U
+#endif
+
+#ifndef PE_BUTTON_TIMEOUT_HOLD_REPEATED
+#define PE_BUTTON_TIMEOUT_HOLD_REPEATED 1200U
+#endif
+
+#ifndef PE_BUTTON_TIMEOUT_REPEAT
+#define PE_BUTTON_TIMEOUT_REPEAT 100U
+#endif
 
 typedef bool (*PE_ButtonReader_t) ();
+typedef bool (*PE_ButtonHandler_t) ();
 
 class PE_Button {
 private:
     PE_ButtonReader_t _reader;
     uint8_t _status = 0;
-    uint32_t _time = 0;
-    void (*_onPress) () = nullptr;
-    void (*_onRelease) ();
-    void (*_onHoldSingular) ();
-    void (*_onHoldRepeated) ();
+    uint32_t _millis = 0;
+    PE_ButtonHandler_t _onPress        = nullptr;
+    PE_ButtonHandler_t _onRelease      = nullptr;
+#if PE_BUTTON_USE_HOLD_SINGULAR == 1
+    PE_ButtonHandler_t _onHoldSingular = nullptr;
+#endif
+#if PE_BUTTON_USE_HOLD_REPEATED == 1
+    PE_ButtonHandler_t _onHoldRepeated = nullptr;
+#endif
 public:
     explicit PE_Button(PE_ButtonReader_t reader);
 
-    void initialize(uint32_t time);
+    /**
+     * Init button internal timestamp
+     *
+     * @param millis
+     */
+    void initialize(uint32_t millis);
 
-    void dispatch(uint32_t time);
+    /**
+     * Dispatch button handlers
+     *
+     * @param millis
+     */
+    void dispatch(uint32_t millis);
+
+    /**
+     * Set handler for press event
+     *
+     * @param handler
+     */
+    void setOnPress(PE_ButtonHandler_t handler);
+
+    /**
+     * Set handler for release event
+     *
+     * @param handler
+     */
+    void setOnRelease(PE_ButtonHandler_t handler);
+
+#if PE_BUTTON_USE_HOLD_SINGULAR == 1
+    /**
+     * Set handler for singular hold event
+     *
+     * @param handler
+     */
+    void setOnHoldSingular(PE_ButtonHandler_t handler);
+#endif
+
+#if PE_BUTTON_USE_HOLD_REPEATED == 1
+    /**
+     * Set handler for repeated hold event
+     *
+     * @param handler
+     */
+    void setOnHoldRepeated(PE_ButtonHandler_t handler);
+#endif
 };
 
 #endif //PE_BUTTON_H
