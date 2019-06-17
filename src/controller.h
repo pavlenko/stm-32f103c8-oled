3496700f8cd2a8024579d48fb7cb0b978236f44c
Pavlenko Eugeniy <pavlenko.obs@gmail.com>
2019-06-17 13:05:34 +0300

    think about controller

---
+++ src/controller.h
@@ -0,0 +1,22 @@
+#ifndef __CONTROLLER_H
+#define __CONTROLLER_H
+
+/**
+ *
+ *     ^ Y            [A]
+ *     |   X
+ * <-- o -->       [D]    [B]
+ *     |
+ *     v    [F] [E]   [C]
+ *
+ * Y - move front/rear
+ * X - rotate ?
+ * A - ?
+ * B - ?
+ * C - ?
+ * D - ?
+ * E - ?
+ * F - ?
+ */
+
+#endif //__CONTROLLER_H
